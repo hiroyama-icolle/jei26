a39e2b4a004d2c45868180d861d7f982326cce1e
hiroyama-icolle <hiroyama@std.it-college.ac.jp>
2026-05-29 12:07:47 +0900

    課題

---
+++ src/d49/Q22Kadai.java
@@ -0,0 +1,7 @@
+package d49;
+
+public class Q22Kadai {
+    static void main(String[] args) {
+        // Q22を参考に続きをつくってみましょう
+    }
+}
---
+++ src/d49/Q23.java
@@ -0,0 +1,15 @@
+package d49;
+
+public class Q23 {
+    static void main(String[] args) {
+        double n = Double.parseDouble(args[2]) * 1.5;
+        System.out.println(n);
+    }
+}
+/*
+端末のコマンドからだと次のような操作となる
+vim Q23.java
+javac Q23.java
+java Q23 2 8 10
+
+ */
