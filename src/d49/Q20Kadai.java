e6eab5c806c38a18090ef01eecdf034bb8f2d55f
hiroyama-icolle <hiroyama@std.it-college.ac.jp>
2026-05-29 10:10:44 +0900

    課題

---
+++ src/d49/Q20Kadai.java
@@ -0,0 +1,9 @@
+package d49;
+
+public class Q20Kadai {
+    static void main(String[] args) {
+        int[] arr = new int[10]; // ここをdouble型の2次元配列にする
+
+        System.out.println(arr.length);
+    }
+}
